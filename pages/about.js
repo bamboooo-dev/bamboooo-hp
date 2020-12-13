d5fc736edbbc2e8a6cd461d1d24edc5cc915be1d
Yuya Sumie <monamonacan0926@gmail.com>
2020-12-13 16:08:27 +0900

    About ページの作成

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,37 @@
 import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
-import Link from '../src/Link';
 
 export default function About() {
   return (
-    <Container maxWidth="sm">
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Philosophy
+      </Typography>
       <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
+        <Typography variant="h5" gutterBottom>
+          新しいエンタメを
         </Typography>
-        <Button variant="contained" color="primary" component={Link} naked href="/">
-          Go to the main page
-        </Button>
+        <Typography variant="body1" gutterBottom>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+      </Box>
+      <Box my={4}>
+        <Typography variant="h5" gutterBottom>
+          好きなことを好きなだけ
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+      </Box>
+      <Box my={4}>
+        <Typography variant="h5" gutterBottom>
+          攻めの姿勢を忘れない
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
       </Box>
     </Container>
   );
