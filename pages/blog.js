f59a12300a0e79f770e299d492bbb56c36f5eb8c
Yuya Sumie <monamonacan0926@gmail.com>
2021-09-02 01:20:57 +0900

    とりあえず実装

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,5 @@
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import { Box, Divider } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
-import Hidden from '@material-ui/core/Hidden';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
@@ -19,12 +15,30 @@ const useStyles = makeStyles((theme) => ({
   cardMedia: {
     width: 320,
   },
-  mainGrid: {
-    marginTop: theme.spacing(3),
-  },
   continueReading: {
     color: theme.palette.tertiary.main,
-  }
+  },
+
+  coverBox: {
+    backgroundImage: 'url(/about_cover.png)',
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+  },
+  innerBox: {
+    maxWidth: '1000px',
+    margin: '0 auto',
+  },
+  whiteBox: {
+    backgroundColor: 'white',
+    paddingTop: theme.spacing(10),
+    paddingLeft: theme.spacing(10),
+    paddingRight: theme.spacing(10),
+    [theme.breakpoints.down(777)]: {
+      paddingLeft: 10,
+      paddingRight: 10,
+      paddingTop: 40,
+    },
+  },
 }));
 
 const urls = [
@@ -41,48 +55,61 @@ export async function getStaticProps() {
   }
 }
 
-function PostCard(props){
-  const classes = useStyles();
-  const { post } = props;
-
-  return(
-    <Grid item xs={12}>
-      <CardActionArea component="a" href={post.url} target="_blank">
-        <Card className={classes.card}>
-          <div className={classes.cardDetails}>
-            <CardContent>
-              <Typography component="h2" variant="h5">
-                {post.title}
-              </Typography>
-              <Typography variant="subtitle1" color="textSecondary">
-                {post.date}
-              </Typography>
-              <Typography variant="subtitle1" color="textPrimary">
-                {post.author}
-              </Typography>
-              <Typography variant="subtitle1" className={classes.continueReading}>
-                Continue reading...
-              </Typography>
-            </CardContent>
-          </div>
-          <Hidden xsDown>
-            <CardMedia className={classes.cardMedia} image={post.image} title={post.imageTitle} />
-          </Hidden>
-        </Card>
-      </CardActionArea>
-    </Grid>
-  )
-}
-
 export default function Blog({ posts }) {
 
   const classes = useStyles();
   
   return (
-    <Grid container spacing={5} className={classes.mainGrid}>
-      {posts.map((post) =>(
-        <PostCard key={post.title} post={post} />
-      ))}
-    </Grid>
+    <>
+      <Box className={classes.coverBox}>
+        <Box pt="25vw" className={classes.innerBox}>
+          <Grid container spacing={5} className={classes.whiteBox}>
+            {posts.map((post) =>(
+              <PostBox key={post.title} post={post} />
+            ))}
+          </Grid>
+          <Box w="100%" display="flex" justifyContent="center" my={8}>
+            <Box>
+              <Typography variant="h4" style={{fontFamily: "Corporate-Logo-Medium-ver2"}}>
+                1
+              </Typography>
+              <Divider style={{backgroundColor: "green", height: 3}}/>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
   );
+}
+
+function PostBox(props){
+  const { post } = props;
+
+  return(
+    <Grid item xs={12} sm={4}>
+      <Box display="flex" flexDirection="column">
+        <img src={post.image} width="100%" style={{marginBottom: 20, borderRadius: 5}}/>
+        <Typography variant="h6" style={{minHeight: 128}}>
+          {post.title}
+        </Typography>
+        <Box display="flex" alignItems="center" width="100%" mt={1} mb={2}>
+          <Box width="40%">
+            <Typography variant="subtitle1">
+              {post.date}
+            </Typography>
+          </Box>
+          <Box style={{color: "green", border: "1px solid", borderRadius: 10, paddingLeft: 15, paddingRight: 15}}>
+            <span>カテゴリ</span>
+          </Box>
+        </Box>
+        <Divider style={{backgroundColor: "grey", marginBottom: 10}} />
+        <Box position="relative">
+          <Typography variant="subtitle1">
+            {post.lead}...
+          </Typography>
+          <a href={post.url} target="_blank" style={{position: "absolute", right: 10, bottom: 5, color: "green", textDecoration: "none"}}>{">>"}</a>
+        </Box>
+      </Box>
+    </Grid>
+  )
 }
