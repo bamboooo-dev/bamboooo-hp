5e54f748ebc0e21691a91976d40eea902daa5326
Yuya Sumie <monamonacan0926@gmail.com>
2021-11-24 19:21:02 +0900

    format

--- pages/works.js
+++ pages/works.js
@@ -1,76 +1,69 @@
-import { FormHelperText, Icon, Paper } from '@material-ui/core';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
+import { useMediaQuery } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
 import grey from "@material-ui/core/colors/grey";
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import React from 'react';
-import paper from '@material-ui/core/Paper';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { useMediaQuery } from '@material-ui/core';
-
-
+import Grid from "@material-ui/core/Grid";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import React from "react";
 
 const useStyles = makeStyles((theme) => ({
-  mainDiv:{
-    zIndex:'1',
+  mainDiv: {
+    zIndex: "1",
     backgroundImage: "url(/header.png)",
     backgroundSize: "contain",
     backgroundRepeat: "no-repeat",
-    [theme.breakpoints.down('777')]: {
+    [theme.breakpoints.down("777")]: {
       backgroundImage: "url(/header.png)",
     },
   },
 
   TopContainer: {
-    maxWidth:'1000px',
-    margin:"0 auto",
-    paddingTop:"25vw",
-    
+    maxWidth: "1000px",
+    margin: "0 auto",
+    paddingTop: "25vw",
   },
-
 
   shadowBox: {
     boxShadow: "0 1px 3px 0 rgba(0, 0, 0, .3)",
     marginBottom: "100px",
-    [theme.breakpoints.down('777')]: {
+    [theme.breakpoints.down("777")]: {
       marginBottom: "50px",
     },
-    backgroundColor:"white",
+    backgroundColor: "white",
   },
 
   content: {
-    width: '100%',
-    display:'flex' ,
-    flexFlow:'column',
+    width: "100%",
+    display: "flex",
+    flexFlow: "column",
     /*flexWrap:'nowraop',
     justifyContent:'sapce-around',*/
-    alignItems:'center',
-    paddingTop:60,
-    paddingBottom:100,
-    [theme.breakpoints.down('sm')]: {
-    paddingTop:10,
-    paddingBottom:10,
+    alignItems: "center",
+    paddingTop: 60,
+    paddingBottom: 100,
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: 10,
+      paddingBottom: 10,
     },
   },
 
   /*上寄せにしたい*/
   media: {
     borderWidth: 1,
-    border: "solid", 
+    border: "solid",
     borderRadius: 5,
     borderColor: grey[300],
-    width: '100%',
-    marginTop:10,
+    width: "100%",
+    marginTop: 10,
 
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       width: 400,
       height: 260,
-      display: 'flex',
+      display: "flex",
     },
   },
-  
+
   /*
   mediaWaiwai: {
     borderWidth: 1,
@@ -89,7 +82,7 @@ const useStyles = makeStyles((theme) => ({
   mainContainer: {
     marginTop: theme.spacing(5),
 
-/*
+    /*
     maxWidth:'1000px',
     margin:"0 auto",
     paddingTop:"25vw",
@@ -101,79 +94,79 @@ const useStyles = makeStyles((theme) => ({
     ここらへんの位置をかえればいい感じになると思う
   },
   */
-
-},
+  },
   description: {
-
-/*これがないほうが改行は変かもしれないけどすっきりする
+    /*これがないほうが改行は変かもしれないけどすっきりする
     whiteSpace: "pre-line",*/
 
     lineHeight: 2,
     width: 400,
-    [theme.breakpoints.down('sm')]: {
-      width:300,
+    [theme.breakpoints.down("sm")]: {
+      width: 300,
     },
-   /* height: 170,*/ 
-    display: 'flex',
-    fontFamily:"ヒラギノ角ゴシック",
+    /* height: 170,*/
+    display: "flex",
+    fontFamily: "ヒラギノ角ゴシック",
   },
 
-  genreIcon:{
-
-    borderRadius:"20px",
-    border:"solid 2px",
-    width:"50%",
-    borderColor:"#19807B",
-    float:"right",
-    padding:"4px",
-    fontSize:"14px",
-    fontFamily:"ヒラギノ角ゴシック",
-    textAlign:"center",
-    [theme.breakpoints.down('777')]: {
-      paddingTop:"2px",
-      fontSize:"10px",
+  genreIcon: {
+    borderRadius: "20px",
+    border: "solid 2px",
+    width: "50%",
+    borderColor: "#19807B",
+    float: "right",
+    padding: "4px",
+    fontSize: "14px",
+    fontFamily: "ヒラギノ角ゴシック",
+    textAlign: "center",
+    [theme.breakpoints.down("777")]: {
+      paddingTop: "2px",
+      fontSize: "10px",
     },
   },
 
-    title:{
-      fontSize:"30px",
-      [theme.breakpoints.down('sm')]: {
-       fontSize:"20px",
-    }},
+  title: {
+    fontSize: "30px",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "20px",
+    },
+  },
 
-    content:{
-      padding:"40px",
-      [theme.breakpoints.down('sm')]: {
-        paddingLeft:"25px",
-        paddingTop:"30px",
+  content: {
+    padding: "40px",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "25px",
+      paddingTop: "30px",
+    },
+  },
 
-      }},
+  contentBox: {
+    display: "flex",
+    [theme.breakpoints.down("sm")]: {
+      display: "flex",
+      flexDirection: "column",
+    },
+  },
 
-    contentBox:{
-          display:"flex",
-        [theme.breakpoints.down('sm')]: {
-          display:"flex" ,
-          flexDirection:"column",
-    },},
-
-    Icon:{
-      [theme.breakpoints.down('sm')]: {
-      display:"flex",
-      flexDirection:"column",
-      textAlign:"center",
-      alignItems:"center",
-    }},
+  Icon: {
+    [theme.breakpoints.down("sm")]: {
+      display: "flex",
+      flexDirection: "column",
+      textAlign: "center",
+      alignItems: "center",
+    },
+  },
 }));
 
 const works = [
-  { 
-    title: 'ワイワイ',
-    catchphrase: 'あなたの価値観でみんながワイワイ',
-   /* url:"https://apps.apple.com/jp/app/%E3%83%AF%E3%82%A4%E3%83%AF%E3%82%A4/id1561027910",
+  {
+    title: "ワイワイ",
+    catchphrase: "あなたの価値観でみんながワイワイ",
+    /* url:"https://apps.apple.com/jp/app/%E3%83%AF%E3%82%A4%E3%83%AF%E3%82%A4/id1561027910",
    公式HP載せたい*/
-    alt: 'Picture of waiwai',
-    image: '/waiwai_work.png',
-    smartphoneimage:'/waiwai.png',
+    alt: "Picture of waiwai",
+    image: "/waiwai_work.png",
+    smartphoneimage: "/waiwai.png",
     description: `
       オンラインでオフライン以上にワイワイする。
       「ワイワイ」はおしゃべりしながらオンライン対戦できる心理ゲームで、
@@ -181,48 +174,51 @@ const works = [
       キャラクター(エモー)もかわいいので、テンションも上がること間違いなし！
       自分の性格、センス、価値観が問われるので仲間内はもちろん、
       初対面の人の事も手っ取り早く知る日ことが出来ます。`,
-    release:'リリース　　2021年 4月',
-    appleUrl: "https://apps.apple.com/jp/app/%E3%83%AF%E3%82%A4%E3%83%AF%E3%82%A4/id1561027910",
-    googleUrl: "https://play.google.com/store/apps/details?id=com.bamboooo.waiwai",
+    release: "リリース　　2021年 4月",
+    appleUrl:
+      "https://apps.apple.com/jp/app/%E3%83%AF%E3%82%A4%E3%83%AF%E3%82%A4/id1561027910",
+    googleUrl:
+      "https://play.google.com/store/apps/details?id=com.bamboooo.waiwai",
     /*backgroundColor:'#D4A7D9',*/
-    genre:'対戦型協力ゲームアプリ',
-    gameUrl:"https://apps.apple.com/jp/app/%E3%83%AF%E3%82%A4%E3%83%AF%E3%82%A4/id1561027910",
+    genre: "対戦型協力ゲームアプリ",
+    gameUrl:
+      "https://apps.apple.com/jp/app/%E3%83%AF%E3%82%A4%E3%83%AF%E3%82%A4/id1561027910",
   },
-  { 
-    title: 'memopic',
-    catchphrase: '旅の思い出をプレイリストで振り返ろう',
-    url: 'https://www.memopic.net',
-    alt: 'Picture of memopic',
-    image: '/memopic_work.png',
-    smartphoneimage: '/memopic_work.png',
+  {
+    title: "memopic",
+    catchphrase: "旅の思い出をプレイリストで振り返ろう",
+    url: "https://www.memopic.net",
+    alt: "Picture of memopic",
+    image: "/memopic_work.png",
+    smartphoneimage: "/memopic_work.png",
     description: `
       旅行中ドライブで聴いてた曲
       あの時流行っていた、好きだった曲から
       思い出を振り返ってみませんか？
       『memopic』はプレイリストで旅を記録する
       新しいサービスです`,
-    release:'リリース　　2020年 9月',
+    release: "リリース　　2020年 9月",
     appleUrl: "https://apps.apple.com/us/app/memopic/id1527852844",
     googleUrl: "https://play.google.com/store/apps/details?id=com.memopic",
     /*backgroundColor:'#F5BD70',*/
-    genre:'旅行の思い出共有アプリ',
-    gameUrl:"https://apps.apple.com/us/app/memopic/id1527852844",
+    genre: "旅行の思い出共有アプリ",
+    gameUrl: "https://apps.apple.com/us/app/memopic/id1527852844",
   },
-  { 
-    title: 'Memorip',
-    catchphrase: '旅行プラン作成・共有サイト',
-    url: 'https://memorip.net',
-    alt: 'Picture of Memorip',
-    image: '/memorip_work.png',
-    smartphoneimage: '/memorip_work.png',
+  {
+    title: "Memorip",
+    catchphrase: "旅行プラン作成・共有サイト",
+    url: "https://memorip.net",
+    alt: "Picture of Memorip",
+    image: "/memorip_work.png",
+    smartphoneimage: "/memorip_work.png",
     description: `
       『Memorip』では直感的にプランを作成したり
       簡単にプランを共有したりすることができます
       これからの旅行のしおりとして、また
       他の人のプランを参考にして作成できます`,
-    release:'リリース　　2019年 12月',
+    release: "リリース　　2019年 12月",
     /*backgroundColor:'#99FFFF',*/
-    genre:'旅行プラン作成アプリ',
+    genre: "旅行プラン作成アプリ",
   },
   /* 新しいアプリがリリースor決定したら追加
   { 
@@ -240,76 +236,85 @@ const works = [
 function Work(props) {
   const classes = useStyles();
   const theme = useTheme();
-  const { work } = props; 
-  const Issmartphone = useMediaQuery((theme)=>
-    theme.breakpoints.down('xs')
-  );
+  const { work } = props;
+  const Issmartphone = useMediaQuery((theme) => theme.breakpoints.down("xs"));
   return (
-   <Grid container>
-     <Grid item container xs={12} md={12} display="flex" >
+    <Grid container>
+      <Grid item container xs={12} md={12} display="flex">
         <Grid item className={classes.content} md={12}>
           <Box className={classes.contentBox}>
             {work.image ? (
-              <Box
-              paddingRight={5}
-              paddingLeft={5}>
-                <img  className={classes.media}
+              <Box paddingRight={5} paddingLeft={5}>
+                <img
+                  className={classes.media}
                   /*src ={work.image}*/
 
-              src = {Issmartphone? (
-                   work.smartphoneimage
-                    ):(
-                   work.image)}
-
+                  src={Issmartphone ? work.smartphoneimage : work.image}
                   alt={work.alt}
                 />
               </Box>
             ) : (
-              <Box
-                className={classes.media}
-                height={260}
-                mb={1}
-              >
-                <Typography variant="h4">
-                  COMING SOON...
-                </Typography>
+              <Box className={classes.media} height={260} mb={1}>
+                <Typography variant="h4">COMING SOON...</Typography>
               </Box>
             )}
-          
-            <Box xs={12}
-             className={classes.content}>
+
+            <Box xs={12} className={classes.content}>
               <div className={classes.Icon}>
-              <div className={classes.genreIcon} /*style={{font: work.font}}*/>
-               {work.genre}
-              </div>            
-              <Typography gutterBottom variant="h4" component="h2" >
-                <a href={work.gameUrl}
-                   target="_blank"
-                   style={{
-                     color:"#19807B",
-                     textDecoration:"none",
-                     fontWeight:"bold",
-                   }} className={classes.title}>
-                 {work.title}
-                </a>
-              </Typography>
+                <div
+                  className={classes.genreIcon} /*style={{font: work.font}}*/
+                >
+                  {work.genre}
+                </div>
+                <Typography gutterBottom variant="h4" component="h2">
+                  <a
+                    href={work.gameUrl}
+                    target="_blank"
+                    style={{
+                      color: "#19807B",
+                      textDecoration: "none",
+                      fontWeight: "bold",
+                    }}
+                    className={classes.title}
+                  >
+                    {work.title}
+                  </a>
+                </Typography>
               </div>
-              <Typography variant="body1" color="textPrimary" component="p"
-               style={{
-                 fontWeight:"bold",
-                 textAlign:"center",
-               }}>
+              <Typography
+                variant="body1"
+                color="textPrimary"
+                component="p"
+                style={{
+                  fontWeight: "bold",
+                  textAlign: "center",
+                }}
+              >
                 {work.catchphrase}
               </Typography>
-              <Typography variant="body2" color="textPrimary" component="p" gutterBottom className={classes.description}>
+              <Typography
+                variant="body2"
+                color="textPrimary"
+                component="p"
+                gutterBottom
+                className={classes.description}
+              >
                 {work.description}
               </Typography>
-              <Typography variant="body3" color="textSecondary" component="p" gutterBottom className={classes.release}>
+              <Typography
+                variant="body3"
+                color="textSecondary"
+                component="p"
+                gutterBottom
+                className={classes.release}
+              >
                 {work.release}
               </Typography>
               {work.url && (
                 <Box my={1}>
-                  <Button variant="outlined" href={work.url} target="_blank">公式サイトへ</Button>
+                  <Button variant="outlined" href={work.url} target="_blank">
+                    公式サイトへ
+                  </Button>
                 </Box>
               )}
               {(work.appleUrl || work.googleUrl) && (
@@ -317,9 +322,7 @@ function Work(props) {
                   {work.appleUrl && (
                     <Box>
                       <a href={work.appleUrl} target="_blank">
-                        <img
-                          src="/app-store-badge.png"
-                          alt="App Store Badge" />
+                        <img src="/app-store-badge.png" alt="App Store Badge" />
                       </a>
                     </Box>
                   )}
@@ -329,18 +332,19 @@ function Work(props) {
                         <img
                           src="/google-play-badge.png"
                           alt="Google Play Store Badge"
-                          height={60} />
+                          height={60}
+                        />
                       </a>
                     </Box>
                   )}
                 </Box>
               )}
-              </Box>
             </Box>
+          </Box>
         </Grid>
+      </Grid>
     </Grid>
-   </Grid>
-  )
+  );
 }
 
 export default function Works() {
@@ -349,26 +353,24 @@ export default function Works() {
   return (
     <Grid container>
       <Grid item xs={12} md={12}>
-   <Box className={classes.mainDiv}>
-    <Grid container>
-     <Grid item xs={12}  md={12}> 
-      <Box className={classes.TopContainer}  >
-   
-       <Box className={classes.shadowBox}>
-        <Box className={classes.mainContainer} >
-         <Grid container spacing={4} >
-          {works.map((work) => (
-            <Work key={work.name} work={work}/>
-          ))}
-          </Grid> 
-         </Box>
+        <Box className={classes.mainDiv}>
+          <Grid container>
+            <Grid item xs={12} md={12}>
+              <Box className={classes.TopContainer}>
+                <Box className={classes.shadowBox}>
+                  <Box className={classes.mainContainer}>
+                    <Grid container spacing={4}>
+                      {works.map((work) => (
+                        <Work key={work.name} work={work} />
+                      ))}
+                    </Grid>
+                  </Box>
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
         </Box>
-       </Box>
       </Grid>
-     </Grid>
-    </Box>
     </Grid>
-    </Grid>
-  )
+  );
 }
-
